e8e927d2a9a75d39fe466533d1fd17d90d4c2f4a
Michael Lynch <ml849@hotmail.com>
2024-03-30 17:20:05 -0400

    Flask Test #2

--- moodio.py
+++ moodio.py
@@ -1,10 +1,42 @@
-from flask import Flask,request 
+from flask import Flask,request,render_template,app
 app = Flask(__name__) 
-@app.route('/', methods=['GET', 'POST']) 
+
+
+@app.route("/") 
+def start():
+    return render_template("homepage.html")
+
+
+@app.route("/h.html")
+def start_h():
+    return render_template("homepage.html")
+
+@app.route("/resources.html") 
+def start_recourses():
+    return render_template("resources.html")
+
+@app.route("/about.html") 
+def start_about():
+    return render_template("about.html")
+
+@app.route("/nodes.html") 
+def start_nodes():
+    return render_template("nodes.html")
+
+
+
+"""
+#Sample for later
+@app.route("/", methods=['GET', 'POST']) 
 def mood_io():
+     print("hi")
      if request.method == 'POST': 
+        render_template("nodes.html")
         return inc(request.form['input']) 
-     else: return -1 
+     else: 
+        render_template("nodes.html")
+        return "<p>Hello, World!</p>"
+"""
 
 
 def inc(x): 
@@ -12,4 +44,3 @@ def inc(x):
     return x 
 
 
-app.run(port=5000)
