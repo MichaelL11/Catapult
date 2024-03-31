4ad7ed81b7c2007ff5fd3aba0ae4e3b14f10818d
Sean Chang <mine97059@gmail.com>
2024-03-30 22:11:06 -0400

    Added model stuff

---
+++ deepEmotion.py
@@ -0,0 +1,67 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Deep_Emotion_3Classes(nn.Module):
+    def __init__(self):
+        '''
+        Deep_Emotion_3Classes class contains the modified network architecture with 3 output classes.
+        '''
+        super(Deep_Emotion_3Classes,self).__init__()
+        self.conv1 = nn.Conv2d(1,10,3)
+        self.conv2 = nn.Conv2d(10,10,3)
+        self.pool2 = nn.MaxPool2d(2,2)
+
+        self.conv3 = nn.Conv2d(10,10,3)
+        self.conv4 = nn.Conv2d(10,10,3)
+        self.pool4 = nn.MaxPool2d(2,2)
+
+        self.norm = nn.BatchNorm2d(10)
+
+        self.fc1 = nn.Linear(810,50)
+        self.fc2 = nn.Linear(50,3)  # Modified to output 3 classes
+
+        self.localization = nn.Sequential(
+            nn.Conv2d(1, 8, kernel_size=7),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True),
+            nn.Conv2d(8, 10, kernel_size=5),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True)
+        )
+
+        self.fc_loc = nn.Sequential(
+            nn.Linear(640, 32),
+            nn.ReLU(True),
+            nn.Linear(32, 3 * 2)
+        )
+        self.fc_loc[2].weight.data.zero_()
+        self.fc_loc[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
+
+    def stn(self, x):
+        xs = self.localization(x)
+        xs = xs.view(-1, 640)
+        theta = self.fc_loc(xs)
+        theta = theta.view(-1, 2, 3)
+
+        grid = F.affine_grid(theta, x.size(),align_corners=True)
+        x = F.grid_sample(x, grid,align_corners=True)
+        return x
+
+    def forward(self,input):
+        out = self.stn(input)
+
+        out = F.relu(self.conv1(out))
+        out = self.conv2(out)
+        out = F.relu(self.pool2(out))
+
+        out = F.relu(self.conv3(out))
+        out = self.norm(self.conv4(out))
+        out = F.relu(self.pool4(out))
+
+        out = F.dropout(out)
+        out = out.view(-1, 810)
+        out = F.relu(self.fc1(out))
+        out = self.fc2(out)
+
+        return out
---
+++ predictFromPicture.py
@@ -0,0 +1,49 @@
+import cv2
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import transforms
+from PIL import Image
+from deepEmotion import Deep_Emotion_3Classes
+
+model=Deep_Emotion_3Classes()
+model.load_state_dict(torch.load('model_weights_3class.pth', map_location=torch.device('cpu')))
+model.eval()
+
+def cropFace(img):
+    # Load the cascade
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    for (x, y, w, h) in faces:
+        out=gray[y:y+h, x:x+w]
+        break
+    cv2.imshow('crop',out)
+    cv2.waitKey(0)
+    return out
+
+
+# 0=Happy, 1=Neutral, 2=Sad
+
+def getPredict(img):
+    emotionDict={0:'Happy', 1:'Neutral', 2:'Sad'}
+    transform = transforms.Compose([
+        transforms.Grayscale(),
+        transforms.Resize((48, 48)),
+        transforms.ToTensor(),
+    ])
+    img = Image.fromarray(img)
+    inputTensor = transform(img).unsqueeze(0)  # Add a batch dimension
+    outputs = model(inputTensor)
+    _, pred = torch.max(outputs,1)
+    predIdx=pred.item()
+    emotion=emotionDict[predIdx]
+    return emotion
+
+
+if __name__ == "__main__":
+    imgPath="sad.jpg"
+    img = cv2.imread(imgPath)
+    cropped=cropFace(img)
+    out=getPredict(cropped)
+    print(out)
